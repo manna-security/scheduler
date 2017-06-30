5410a4c61ae1f359a02c18b38974319c1d390da8
jtmelton <jtmelton@gmail.com>
2017-06-30 03:17:18 -0400

    code refactor and adding base classes

--- src/main/java/org/mannasecurity/SchedulerApplication.java
+++ src/main/java/org/mannasecurity/SchedulerApplication.java
@@ -1,10 +1,19 @@
 package org.mannasecurity;
 
+import org.mannasecurity.processing.NoopProcessor;
+import org.mannasecurity.redis.TaskProcessorManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchedulerApplication {
+
+    @Autowired
+    NoopProcessor noopProcessor;
+
+    @Autowired
+    TaskProcessorManager taskProcessorManager;
 
     public static void main(String[] args) {
         SpringApplication.run(SchedulerApplication.class, args);
---
+++ src/main/java/org/mannasecurity/scheduling/SchedulingManager.java
@@ -0,0 +1,52 @@
+package org.mannasecurity.scheduling;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.mannasecurity.processing.NoopProcessor;
+import org.mannasecurity.processing.TaskProcessor;
+import org.mannasecurity.redis.TaskProcessorManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jtmelton on 6/30/17.
+ */
+@Component
+public class SchedulingManager {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private final NoopProcessor noopProcessor;
+
+    private final TaskProcessorManager taskProcessorManager;
+
+    @Autowired
+    public SchedulingManager(final NoopProcessor noopProcessor,
+                             final TaskProcessorManager taskProcessorManager) {
+        this.noopProcessor = noopProcessor;
+        this.taskProcessorManager = taskProcessorManager;
+    }
+
+    @PostConstruct
+    public void initialize() {
+        Map<String, TaskProcessor> processorMap = new HashMap<>();
+
+        taskProcessorManager.setChannelProcessorMap(processorMap);
+
+        taskProcessorManager.start();
+
+        log.debug("Stopped scheduling manager.");
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        taskProcessorManager.stop();
+
+        log.debug("Stopped scheduling manager.");
+    }
+
+}
